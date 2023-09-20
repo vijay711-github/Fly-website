dd21291eee43911bb6ee2dfe1019ee1dde6e2ec6
vijay711-github <vj7112000@gmail.com>
2023-09-20 23:19:16 +0530

    Check

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
+
 import './App.css'
 import Header from './components/Header'
 import MainConatiner from './components/MainConatiner'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
