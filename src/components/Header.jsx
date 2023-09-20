ded62f33681a08e95261baeede1881d723b9533c
vijay711-github <vj7112000@gmail.com>
2023-09-20 21:19:06 +0530

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 			<div className="flex justify-between p-5 ">
 				<div>
 					<img
-						src="/src/assets/flylogo.png "
+						src="/src/assets/react.svg "
 						className="w-25 h-20"
 						alt=" logo"
 					/>
